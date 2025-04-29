9df7216bc4e74c6231bd486130c481c865d41f28
Sulmurz <kamil.zaber@gmail.com>
2025-04-29 23:23:26 +0200

    alternative method of generating ingredients data sections

--- src/main/java/com/solutions/sulmurz/nutricalc/NutriCalcModel.java
+++ src/main/java/com/solutions/sulmurz/nutricalc/NutriCalcModel.java
@@ -7,9 +7,54 @@ import com.solutions.sulmurz.nutricalc.models.PlanModel;
 import java.util.HashMap;
 
 public class NutriCalcModel {
+<<<<<<< Updated upstream
     HashMap<IngredientModel, String> ingredientsList;
     HashMap<MealModel, String> mealsList;
     HashMap<PlanModel, String> plansList;
     String[] vitaminsSet = {"Vitamin A", "Vitamin D", "Vitamin E", "Vitamin K", "Vitamin C", "Vitamin B1", "Vitamin B2", "Vitamin B3", "Vitamin B5", "Vitamin B6", "Vitamin B9", "Vitamin B12"};
     String[] mineralsSet = {"Calcium", "Chloride", "Potassium", "Phosphorus", "Magnesium", "Sodium", "Iron", "Zinc", "Copper", "Manganese", "Molybdenum", "Iodine", "Fluoride", "Chromium",  "Selenium"};
+=======
+    private static ObjectMapper objectMapper;
+    private static List<IngredientModel> ingredientsList;
+    private static HashMap<MealModel, String> mealsList;
+    private static HashMap<PlanModel, String> plansList;
+    private final static String[] macroSet = {"Proteins", "Fats", "Carbohydrates", "Fiber", "KCal"};
+    private final static String[] vitaminsSet = {"Vitamin A", "Vitamin D", "Vitamin E", "Vitamin K", "Vitamin C", "Vitamin B1", "Vitamin B2", "Vitamin B3", "Vitamin B5", "Vitamin B6", "Vitamin B9", "Vitamin B12", "Choline"};
+    private final static String[] mineralsSet = {"Calcium", "Chloride", "Potassium", "Phosphorus", "Magnesium", "Sodium", "Iron", "Zinc", "Copper", "Manganese", "Molybdenum", "Iodine", "Fluoride", "Chromium",  "Selenium"};
+
+    public NutriCalcModel() {
+        ingredientsList = null;
+        mealsList = null;
+        plansList = null;
+    }
+
+    public static List<IngredientModel> getIngredientsList() {
+        return ingredientsList;
+    }
+
+    public static String[] getMacroSet() {
+        return macroSet;
+    }
+
+    public static String[] getVitaminsSet() {
+        return vitaminsSet;
+    }
+
+    public static String[] getMineralsSet() {
+        return mineralsSet;
+    }
+
+    public static void loadIngredientsDatabase() {
+        if(ingredientsList == null) {
+            Path path = Paths.get("C:\\Users\\pc\\IdeaProjects\\NutriCalc\\src\\main\\resources\\nutriCalc_database.json");
+            Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+            Type listType = new TypeToken<List<IngredientModel>>(){}.getType();
+            try {
+                ingredientsList = gson.fromJson(Files.readString(path),listType);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+>>>>>>> Stashed changes
 }
--- src/main/java/com/solutions/sulmurz/nutricalc/controllers/MyIngredientsController.java
+++ src/main/java/com/solutions/sulmurz/nutricalc/controllers/MyIngredientsController.java
@@ -1,5 +1,6 @@
 package com.solutions.sulmurz.nutricalc.controllers;
 
+<<<<<<< Updated upstream
 import com.solutions.sulmurz.nutricalc.NutriCalcMain;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -19,5 +20,68 @@ public class MyIngredientsController {
         root = FXMLLoader.load(getClass().getClassLoader().getResource("main_menu_view.fxml"));
         scene = new Scene(root);
         NutriCalcMain.getPrimaryStage().setScene(scene);
+=======
+import com.solutions.sulmurz.nutricalc.NutriCalcModel;
+import com.solutions.sulmurz.nutricalc.models.IngredientModel;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+
+public class MyIngredientsController extends NutriCalcController {
+    @FXML
+    private ListView<IngredientModel> ingredientsListView;
+    @FXML
+    private Label nameTextField;
+    @FXML
+    private VBox macroAmountsVBox;
+    @FXML
+    private VBox mineralsAmountsVBox;
+    @FXML
+    private VBox vitaminsAmountsVBox;
+
+    @FXML
+    private void initialize() {
+        ingredientsListView.getItems().setAll(NutriCalcModel.getIngredientsList());
+        ingredientsListView.setCellFactory(ingredientsListView -> new ListCell<>() {        //nowe komórki o określonym działaniu
+            @Override
+            protected void updateItem(IngredientModel ingredient, boolean empty) {
+                super.updateItem(ingredient, empty);                    //nazywanie komórek...
+                if(empty || ingredient == null) {
+                    setText(null);
+                } else {
+                    setText(ingredient.getName());                      //po "nazwie" w obiekcie składnika
+                }
+            }
+        });
+
+    }
+
+    private void generateSection(String name, VBox container, String[] fieldsNames, float[] values) {
+        int i = 0;
+        for(Node node: container.getChildren()) {
+            if(node instanceof TextField) {
+                ((TextField) node).setText(name);
+            } else if(node instanceof Label) {
+                ((Label) node).setText(fieldsNames[i] + ": " + values[i]);
+                i++;
+            }
+            node.setVisible(true);
+        }
+    }
+
+    @FXML
+    private void onMouseClicked() {
+        IngredientModel ingredient = ingredientsListView.getSelectionModel().getSelectedItem();
+
+        if (ingredient != null) {
+            nameTextField.setVisible(true);
+            nameTextField.setText(ingredient.getName());
+
+            generateSection("MACRO: ", macroAmountsVBox, NutriCalcModel.getMacroSet(), ingredient.getMacroAmounts());
+            generateSection("MINERALS: ", mineralsAmountsVBox, NutriCalcModel.getMineralsSet(), ingredient.getMineralsAmounts());
+            generateSection("VITAMINS: ", vitaminsAmountsVBox, NutriCalcModel.getVitaminsSet(), ingredient.getVitaminsAmounts());
+        }
+>>>>>>> Stashed changes
     }
 }
